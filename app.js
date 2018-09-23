f1fbf277d22610fdebeb5bb911439d8d0498966c
kmasalam <mccyber007@gmail.com>
2018-09-24 00:11:00 +0600

    updating game target point

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
 		//Check if Player won the game
-		if(scores[activePlayer] >= '20'){
+		if(scores[activePlayer] >= '100'){
 			document.getElementById('name-' + activePlayer).textContent = 'Winner';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			gamePlaying = false;
